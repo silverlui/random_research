2cd51e74f29738f3f1663d5a6c5d6b2abdaa95f9
Luis Cabrera <80076244+silverlui@users.noreply.github.com>
2021-11-02 22:54:38 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,13 @@
 # from a free fall experiment in Physics Lab.
 
 # Sources:
+# Luis Cabrera
+
+# This program is intended to calculate the mean of
+# given data and its standard error. The idea is derived
+# from a free fall experiment in Physics Lab.
+
+# Sources:
 # https://mathworld.wolfram.com/StandardError.html
 # https://www.mathsisfun.com/data/standard-deviation-formulas.html
 # https://numpy.org/devdocs/user/absolute_beginners.html
@@ -12,11 +19,12 @@
 # https://en.m.wikipedia.org/wiki/Linear_congruential_generator
 # https://apastyle.apa.org/style-grammar-guidelines/tables-figures/tables
 
-import time
-import functools
+import matplotlib.pyplot as plt
 import numpy as np
+from collections import Counter
+from time import time
 from math import sqrt
-
+from functools import wraps
 
 # Code Structure:
 # ---------------
@@ -28,7 +36,7 @@ from math import sqrt
 
 
 # I've found memoizing to be super useful especially
-# since many algo inevitably compute same arithmetic
+# since many algo inevitablly compute same arithmetic
 def memoize(func):
     '''
     Desc:
@@ -42,84 +50,120 @@ def memoize(func):
     (Any) - The result of memoized function
     '''
     cache = {}  # memory
-
+ 
     # bug: search only allows for 1 parameter
-    @functools.wraps(func)  # allows the return of function within function
+    @wraps(func)  # allows the return of function within function
     def search(items):
         if items not in cache:
             cache[items] = func(items)  # Assign new computation
         return cache[items]  # and return
-
+ 
     return search
-
 
 # -----------------------------------------------------------
 # DATA GENERATION
 # -----------------------------------------------------------
 
 
-# I've decided to create an random function to
-# demystify how random works and learn how to
-# design an algorithm.
+
+# I've decided to create an random function to 
+# demistify how random works and learn how to
+# desgin an algorithm.
 
 # @memoize # 'decorator' integrates into function
 def my_random(length: int, nums_in_range: int):
     """
     Desc:
     The idea from my algo is derived from linear congruential algorithm
-    and use the of time as initial value; the function is able to generates
-    itself. Assuming a perfectly random coin flip has a mean of 5 over 10 attempts.
+    and use the of time as intial value; the function is able to generates 
+    itself. Assuming a perfectly random coin flip has a mean of 5 over 10 attempts. 
     My function seems to approach (5.5) this mean, although im unsure what
     kind of distribution the numbers should have.
     Params:
     length (int) - length of the list
     nums_in_range (int) - numbers that appear in list
     Returns:
-    (list) - pseudo random list
+    (list) - psuedo random list
     """
     # initialize timer
-    rand = time.time()
-    a = time.time() - 1
-    c = time.time() - 2
+    rand = time()
+    a = time()-1
+    c = time()-2
 
     rand_list = np.arange(length)
     for i in np.nditer(rand_list):
         rand = (a * rand + c) % nums_in_range
-        rand_list[i] = rand + 1
+        rand_list[i] = rand+1
 
     return rand_list
 
 
-# sorting data beforehand
 
-start_time = time.time()
-data = np.sort(my_random(10, 10))
-total_time = time.time() - start_time
+
+# sorting data beforehand 
+
+start_time = time()
+data = np.sort(my_random(1000000, 100))
+total_time = time() - start_time
 
 # Numpy random number generator to simulate large scale data
-generate = np.random.randint(10, size=10)
+generate = np.random.randint(100, size=1000000)    
 
 
 # -----------------------------------------------------------
 # MAIN FUNCTION
-# -----------------------------------------------------------
+# ----------------------------------------------------------- 
 
 def main():
     print("\t\tWelcome to my Data Analysis Project")
     print("-" * 63)
     global data
-    user_choice = int(input("Numpy's Random [1]\nMy Random      [2] -->"))
-    if user_choice == 2:
-        print_summary(DataItems, 50, 10)
-        print(data)
-        bar_graph(data, 10)
-    elif user_choice == 1:
-        data = generate
-        print_summary(DataItems, 50, 10)
-        print(data)
-        bar_graph(data, 10)
+    print_summary(DataItems, 50, 10)
+    print("data", data)
+    
 
+
+    # numpy_randint
+    lists_1 = sorted(count(generate).items())
+
+    x_1, y_1 = zip(*lists_1)
+
+    
+
+
+    # my_random
+    lists_2 = sorted(count(data).items())
+
+    x_2, y_2 = zip(*lists_2)
+
+
+
+    plt.subplot()
+    numpy_rand = plt.plot(x_1, y_1)
+    plt.setp(numpy_rand, color='blue', linewidth=1)
+    plt.title("Distribution Chart")
+    plt.xlabel("Numbers in Range")
+    plt.ylabel("Frequency")
+
+    
+    plt.subplot()
+    my_rand = plt.plot(x_2, y_2)
+    plt.setp(my_rand, color='red', linewidth=1)
+    plt.show()
+
+    # user_choice = int(input("Numpy's Random [1]\nMy Random      [2] -->"))
+    # if user_choice == 2:
+    #     
+
+    # elif user_choice == 1:
+    #     data = generate
+    #     print_summary(DataItems, 50, 10)
+    #     print(data)
+
+    
     return print("\n[{}] seconds...".format(total_time))
+    
+
 
 
 # -----------------------------------------------------------
@@ -127,7 +171,7 @@ def main():
 # -----------------------------------------------------------
 
 
-def sample_s_deviation(a: list):
+def sample_s_deviation(a : list):
     '''
     Desc:
     Perform (dev)iance calculation for every element in list.
@@ -137,13 +181,14 @@ def sample_s_deviation(a: list):
     (int) - square root of new mean
     '''
 
+
     # Numpy's mechanism called Broadcasting
     # allowed me to perform arithmetic to every cell
     # in array resulting in a significant performance boost
     # compared to the slow python for loop. (looping occurs in c)
     new_arr = (a - np.mean(a)) ** 2
     new_mean = np.mean(new_arr)
-    samp_var = sqrt(new_mean)  # readability
+    samp_var = sqrt(new_mean) #readbility
     return samp_var
 
 
@@ -160,8 +205,23 @@ def standard_error(a):
     return samp_var / sqrt(a.size)
 
 
-# Create a recursive distribution function
-# that takes advantage of memoization
+# occurance function
+
+def count(a):
+    count = Counter()
+    for nums in a:   
+        count[nums] += 1
+    return dict(count)
+
+
+
+
+
+# Create Diehard test functions
+# that operate on my custom random
+# number generator
+
+
 
 
 # -----------------------------------------------------------
@@ -169,8 +229,8 @@ def standard_error(a):
 # -----------------------------------------------------------
 
 
-def print_summary(a: dict, left_width: int, right_width: int):
-    '''
+def print_summary(a: dict, left_width : int, right_width: int):
+    ''' 
     Desc:
     Name of Table using the center method
     with arguments passed in the last two parameters.
@@ -181,37 +241,22 @@ def print_summary(a: dict, left_width: int, right_width: int):
     left_width - (int) an integer that defines the spacing of function from left side
     right_width - (int) - an integer that defines the spacing of function from right side
     Returns:
-    (prints) - an organized dictionary
+    (prints) - an organized dictioary 
     '''
     table_width = right_width + left_width
-    print("Summary of Dataset".center(left_width + (right_width + 3), "_"))
+    print("Summary of Dataset".center(left_width + (right_width+3), "_"))
     for i, j in a.items():
         print("|", i.ljust(left_width, " ") + str(j).rjust(right_width) + '|')
-        print("|" + "-" * (table_width + 1) + "|")
+        print("|"+ "-"*(table_width+1)+ "|")
 
 
-# Another form of visualization via a graph function.
-# Matplotlib is a million times better but here is a
-# custom version of mine
-def bar_graph(data, detail):
-    """
-    WIP
-    """
-    bar = chr(9608)
 
-    # print the y - axis of graph
-    for y in reversed(range(detail)):
-        print(y, "|",
-              bar, bar, bar, bar, bar,
-              bar, bar, bar, bar, bar)
+    
 
-    # print the x - axis of graph
-    print("   ", '__' * len(data))
-    for x in range(len(data)):
-        if x == 0:
-            print("   ", x, end=" ")
-        else:
-            print(x, end=" ")
+
+
+
+
 
 
 # A dictionary of all of the attributes is an easy
